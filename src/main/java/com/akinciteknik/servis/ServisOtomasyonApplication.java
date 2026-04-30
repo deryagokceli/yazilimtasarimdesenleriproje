2b59160f36f2d5fc0a56c73b4f855542eb8d0f57
Derya Gökçeli <deryagokceli5957@gmail.com>
2026-04-30 13:38:01 +0300

    feat: proje iskeleti sıfırdan kuruldu

---
+++ src/main/java/com/akinciteknik/servis/ServisOtomasyonApplication.java
@@ -0,0 +1,13 @@
+package com.akinciteknik.servis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServisOtomasyonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServisOtomasyonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akinciteknik/servis/ServisOtomasyonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akinciteknik.servis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServisOtomasyonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
